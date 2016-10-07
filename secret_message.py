67535309efc47c3adc07cad583f872d1873c6744
Joshua Barnett <Joshua Barnett>
2016-10-07 15:39:58 -0600

    Working program

--- secret_message.py
+++ secret_message.py
@@ -3,4 +3,14 @@ def rename_files():
     file_list = os.listdir(r"D:\Programming\Python\secret_message\Photos\prank")
     print(file_list)
 
+    saved_path = os.getcwd()
+    os.chdir(r"D:\Programming\Python\secret_message\Photos\prank")
+
+    for file_name in file_list:
+        print("Old Name - " + file_name)
+        print("New Name - " + file_name.translate(None, "0123456789"))
+        os.rename(file_name, file_name.translate(None, "0123456789"))
+
+    os.chdir(saved_path)    
+
 rename_files()    
